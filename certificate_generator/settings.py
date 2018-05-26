23c3d22cb7dd3a588e4b859dda467344d6e49bea
sarang <sarang05me43@gmail.com>
2018-05-26 11:51:54 +0530

    commented out logging in settings.py

--- certificate_generator/settings.py
+++ certificate_generator/settings.py
@@ -12,12 +12,15 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
+"""
 # importing logger settings
 try:
     from .logger_settings import *
 except Exception as e:
     # in case of any error, pass silently.
     pass
+
+"""
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
